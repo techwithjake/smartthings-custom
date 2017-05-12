f57f6da8702da9fb8430e350b0a32e5036434aa9
Jake Kromer <im@techwithjake.com>
2017-05-12 16:16:25 -0700

    Adding Meta Data

--- smartapps/techwithjake/auto-door-lock.src/auto-door-lock.groovy
+++ smartapps/techwithjake/auto-door-lock.src/auto-door-lock.groovy
@@ -1,3 +1,27 @@
+/**
+ *  Copyright 2017 SmartThings
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Auto Lock Door
+ *
+ *  Author: TechWithJake, NateBentley, SmartThings
+ *  Date: 2017 - 05 - 12
+ *
+ * Change Log:
+ *	1. Creating Auto Door Lock based on Enhanced Auto Door Lock
+ *   1.2 Removing Unlock options
+ *   1.3 Adding Lock option based on Unlock time
+ *
+ */
+
 definition(
     name: "Auto Lock Door",
     namespace: "techwithjake",
